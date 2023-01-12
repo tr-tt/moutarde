37b1829eb2ebe68b28685a66e9b30b3461663091
ghyslaine ribeiro <ghyslaine.ribeiro.freelance@gmail.com>
2023-01-12 19:32:05 +0100

    image max size 10MB

--- sources/frontend/components/MOU_input_picture/MOU_input_picture.js
+++ sources/frontend/components/MOU_input_picture/MOU_input_picture.js
@@ -21,6 +21,11 @@ class MOUInputPicture extends HTMLElement
 
         this._file0 = null
         this._file1 = null
+
+        this._maxSize = 1048576 * 10 // 10MB in Bytes binary
+        this._maxWidth = 800
+        this._maxHeight = 600
+        this._quality = 0.6
     }
 
     connectedCallback()
@@ -35,41 +40,135 @@ class MOUInputPicture extends HTMLElement
 
     disconnectedCallback()
     {
+        this._input0.removeEventListener('change', this._onChangeInput0Handler)
+        this._input1.removeEventListener('change', this._onChangeInput1Handler)
 
-        
+        this._delete0.removeEventListener('click', this._onDelete0Handler)
+        this._delete1.removeEventListener('click', this._onDelete1Handler)
+    }
+
+    _resizeImg(srcFile)
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const reader = new FileReader()
+
+            reader.onload = (event) =>
+            {
+                const img = document.createElement('img')
+                const canvas = document.createElement('canvas')
+                const ctx = canvas.getContext('2d')
+    
+                img.src = event.target.result
+    
+                img.onload = () =>
+                {
+                    let width = img.width
+                    let height = img.height
+    
+                    if(width > height)
+                    {
+                        if (width > this._maxWidth)
+                        {
+                            height *= this._maxWidth / width
+                            width = this._maxWidth
+                        }
+                    }
+                    else
+                    {
+                        if(height > this._maxHeight)
+                        {
+                            width *= this._maxHeight / height
+                            height = this._maxHeight
+                        }
+                    }
+    
+                    canvas.width = width
+                    canvas.height = height
+                    ctx.drawImage(img, 0, 0, width, height)
+    
+                    canvas.toBlob(
+                        (blob) =>
+                        {
+                            const file = new File([blob], srcFile.name)
+            
+                            if(file.size < this._maxSize)
+                            {
+                                resolve(file)
+                            }
+                            else
+                            {
+                                reject('Taille max 10MB')
+                            }
+                        },
+                        srcFile.type,
+                        this._quality
+                    )
+                }
+            }
+    
+            if(srcFile.size > this._maxSize)
+            {
+                reader.readAsDataURL(srcFile)
+            }
+            else
+            {
+                resolve(srcFile)
+            }
+        })
     }
 
     _onChangeInput0Handler()
     {
-        const [file] = this._input0.files
-        
-        if(file)
+        const [srcFile] = this._input0.files
+
+        if(srcFile)
         {
-            this._img0.src = URL.createObjectURL(file)
-            this._img0.classList.remove('hide')
+            this._resizeImg(srcFile)
+                .then((dstFile) =>
+                    {
+                        this._file0 = dstFile
 
-            this._delete0.setAttribute('label', 'Supprimer')
-            this._delete0.setAttribute('css', 'default')
-            this._delete0.classList.add('above')
+                        this._img0.src = URL.createObjectURL(this._file0)
+                        this._img0.classList.remove('hide')
 
-            this._file0 = file
+                        this._delete0.setAttribute('label', 'Supprimer')
+                        this._delete0.setAttribute('css', 'default')
+                        this._delete0.classList.add('above')
+
+                    })
+                    .catch((exception) =>
+                    {
+                        this._delete0.setAttribute('label', exception)
+                        this._delete0.setAttribute('css', 'error')
+                    })
         }
     }
 
     _onChangeInput1Handler()
     {
-        const [file] = this._input1.files
-        
-        if(file)
+        const [srcFile] = this._input1.files
+
+        if(srcFile)
         {
-            this._img1.src = URL.createObjectURL(file)
-            this._img1.classList.remove('hide')
+            this._resizeImg(srcFile)
+                .then((dstFile) =>
+                    {
+                        this._file1 = dstFile
 
-            this._delete1.setAttribute('label', 'Supprimer')
-            this._delete1.setAttribute('css', 'default')
-            this._delete1.classList.add('above')
+                        this._img1.src = URL.createObjectURL(this._file1)
+                        this._img1.classList.remove('hide')
 
-            this._file1 = file
+                        this._delete1.setAttribute('label', 'Supprimer')
+                        this._delete1.setAttribute('css', 'default')
+                        this._delete1.classList.add('above')
+
+                    })
+                    .catch((exception) =>
+                    {
+                        this._delete1.setAttribute('label', exception)
+                        this._delete1.setAttribute('css', 'error')
+                    })
         }
     }
 
